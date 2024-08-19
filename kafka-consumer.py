5286967933a7ddcb69dca2f6832d75f7ea1c306a
Mihai Mihailescu <mihaimihailescu@Mihais-MacBook-Air.local>
2024-08-19 23:27:04 +0300

    added metadata

--- kafka-consumer.py
+++ kafka-consumer.py
@@ -3,21 +3,33 @@ import numpy as np
 import cv2
 import pickle
 import base64
-# Configuration for Kafka Consumer
-conf = {
-    "bootstrap.servers": "localhost:9092",  # Replace with your Kafka broker's address
-    "group.id": "my_consumer_group",  # Consumer group ID
-    "auto.offset.reset": "latest",  # Start reading at the earliest offset
-}
+import argparse
 
-# Create Consumer instance
-consumer = Consumer(conf)
 
-# Subscribe to a topic
-topic = "test_topic"
-consumer.subscribe([topic])
+def draw_labels(frame, metadata):
+    for label in metadata["labels"]:
+        x1, y1, x2, y2 = label["coordinates"]
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-try:
+    return frame
+
+
+def main(args):
+    hafka_host = args.kafka_host
+    # Configuration for Kafka Consumer
+    conf = {
+        "bootstrap.servers": hafka_host,  # Replace with your Kafka broker's address
+        "group.id": "my_consumer_group",  # Consumer group ID
+        "auto.offset.reset": "latest",  # Start reading at the earliest offset
+    }
+
+    # Create Consumer instance
+    consumer = Consumer(conf)
+
+    # Subscribe to a topic
+    topic = "test_topic"
+    consumer.subscribe([topic])
+
     while True:
         # Poll for a message
         msg = consumer.poll(timeout=1.0)
@@ -29,10 +41,19 @@ try:
         image_bytes = base64.b64decode(buffer)
         image_np = np.frombuffer(image_bytes, np.uint8)
         frame = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+        frame = draw_labels(frame, data["metadata"])
         cv2.imshow(stream, frame)
         cv2.waitKey(1)
 
-        print(f"Received {len(msg.value())} bytes from {msg.topic()}  [{msg.partition()}] offset {msg.offset()}]")
-finally:
-    # Close down consumer to commit final offsets
-    consumer.close()
+        print(
+            f"Received {len(msg.value())} bytes from {msg.topic()}  [{msg.partition()}] offset {msg.offset()}]"
+        )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--kafka_host", type=str, default="2.tcp.eu.ngrok.io:17273", help="Kafka host"
+    )
+    args = parser.parse_args()
+    main(args)
--- kafka-producer.py
+++ kafka-producer.py
@@ -5,11 +5,13 @@ import pickle
 import base64
 import argparse
 import time
+import random
 
 
-FPS = 60
+FPS = 30
 DELAY = 1 / FPS
 IMAGE_WIDTH = 1000
+
 
 # Function to handle delivery reports
 def delivery_report(err, msg):
@@ -18,15 +20,31 @@ def delivery_report(err, msg):
     else:
         print(f"Message delivered to {msg.topic()} [{msg.partition()}]")
 
+
+def frame_analysis(frame):
+    # code for recognition come here
+    # labels must be added also here
+    height, width = frame.shape[:2]
+    metadata = {}
+    labels = []
+    for i in range(random.randrange(1, 10)):
+        x1 = random.randrange(1, width)
+        y1 = random.randrange(1, height)
+        x2 = random.randrange(x1, width)
+        y2 = random.randrange(y1, height)
+        labels.append({"label": f"label_{i}", "coordinates": [x1, y1, x2, y2]})
+
+    metadata["labels"] = labels
+    metadata["timestamp"] = time.time()
+    return metadata
+
+
 def main(args):
-    
-    
     video = args.video
     topic = args.topic
     stream = args.stream
-    kafka_host = args.kafka_host 
-    
-    
+    kafka_host = args.kafka_host
+
     # Configuration for Kafka Producer
     conf = {
         "bootstrap.servers": kafka_host,  # Replace with your Kafka broker's address
@@ -42,22 +60,30 @@ def main(args):
             cap = cv2.VideoCapture(video)
             ret, frame = cap.read()
         frame = imutils.resize(frame, width=IMAGE_WIDTH)
+        metadata = frame_analysis(frame)
         _, buffer = cv2.imencode(".jpg", frame)
         image = base64.b64encode(buffer).decode()
         data["image"] = image
+        data["metadata"] = metadata
         value = pickle.dumps(data)
         # Produce a message to a specific topic
         producer.produce(topic, key="key", value=value, callback=delivery_report)
         # Wait for all messages in the producer queue to be delivered
         producer.flush()
         time.sleep(DELAY)
-    
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--topic", type=str, default="test_topic", help="Topic name")
-    parser.add_argument("--video", type=str, default="intersection_3.mp4", help="Video file path")
-    parser.add_argument("--stream", type=str, default="my_name", help="name of the stream")
-    parser.add_argument("--kafka_host", type=str, default="5.tcp.eu.ngrok.io:13192", help="Kafka host")
+    parser.add_argument(
+        "--video", type=str, default="intersection_3.mp4", help="Video file path"
+    )
+    parser.add_argument(
+        "--stream", type=str, default="my_name", help="name of the stream"
+    )
+    parser.add_argument(
+        "--kafka_host", type=str, default="2.tcp.eu.ngrok.io:17273", help="Kafka host"
+    )
     args = parser.parse_args()
-    main(args)  
-    
+    main(args)
